3f10478e9a0af7090a9c949267a1b5a53caff6a2
YutoTakahashi <yuto_takahashi@bita.jp>
2020-03-04 21:17:29 +0900

    update: storeとりあえず

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,34 @@
-import React, { FC } from "react";
+import React, { FC, useReducer, Dispatch } from "react";
 import Default from "../components/templates/Default";
+import {
+  setDeckInitialize,
+  initialState,
+  reducer,
+  State
+} from "../domain/store/blackjack";
+import getInitialCard from "../domain/logics/cardInitialize"
 
-const Home: FC = () => <Default>hoge</Default>;
+export const Store = React.createContext<{
+  state: State;
+  dispatch: Dispatch<any>;
+}>({
+  state: initialState,
+  dispatch: (value: any) => value
+});
+
+const Home: FC = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <Store.Provider value={{ state, dispatch }}>
+      <Default>
+        <p onClick={() => setDeckInitialize()}>blackjack</p>
+        <p onClick={() => {
+          getInitialCard(2)
+        }}>hogehgoe</p>
+      </Default>
+    </Store.Provider>
+  );
+};
 
 export default Home;

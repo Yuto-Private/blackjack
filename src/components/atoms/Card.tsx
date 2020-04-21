fe4bca82056314b3fa3c37aa8bae7edde319e4f8
YutoTakahashi <yuto_takahashi@bita.jp>
2020-04-21 20:02:06 +0900

    update: ハートとダイアのカードは赤くする

--- src/components/atoms/Card.tsx
+++ src/components/atoms/Card.tsx
@@ -12,8 +12,12 @@ const Card: FC<Props> = ({ value, isHidden }) => {
   const [suit, number] = value;
   return (
     <Component>
-      <Suit>{isHidden ? "?" : cardSuitScreen[suit]}</Suit>
-      <Number>{isHidden ? "?" : cardNumberScreen[number]}</Number>
+      <Suit suitColor={suit} isHidden={isHidden}>
+        {isHidden ? "?" : cardSuitScreen[suit]}
+      </Suit>
+      <Number suitColor={suit} isHidden={isHidden}>
+        {isHidden ? "?" : cardNumberScreen[number]}
+      </Number>
     </Component>
   );
 };
@@ -27,18 +31,22 @@ const Component = styled.dl`
   }
 `;
 
-const Suit = styled.dt`
+const Suit = styled.dt<{ suitColor: number; isHidden?: boolean }>`
   position: absolute;
   top: 0;
   left: 0;
   font-size: 10px;
+  color: ${({ suitColor, isHidden }) =>
+    !isHidden && 2 < suitColor ? `#f00` : "#000"};
 `;
 
-const Number = styled.dd`
+const Number = styled.dd<{ suitColor: number; isHidden?: boolean }>`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  color: ${({ suitColor, isHidden }) =>
+    !isHidden && 2 < suitColor ? `#f00` : "#000"};
 `;
 
 export default Card;
